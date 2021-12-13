954fee884bec32652e4aa4e86092d42aa1f12109
Mohammad Haroun <harounmohammad@yahoo.com>
2021-12-13 06:45:38 +0200

    finish

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function header(props) {
+    const { incomplete } = props;
+    return (
+        <header>
+            <h1>To List Manager</h1>
+            <h2>To Do List: {incomplete} </h2>
+        </header>
+    )
+}
+
+export default header
---
+++ src/components/list/list.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, Card, Elevation } from "@blueprintjs/core";
+
+export default function List(props) {
+    const { item, toggleComplete } = props;
+    return (
+        <div >
+            <Card interactive={true} elevation={Elevation.TWO}>
+                <h5><a href="#">{item.text}</a></h5>
+                <p>Assigned to: {item.assignee}</p>
+
+                
+                <p>Difficulty: {item.difficulty}</p>
+                Completed: <Button  onClick={() => toggleComplete(item.id)}>{item.complete.toString()}</Button>
+            </Card>
+            {/* <hr /> */}
+            <br/>
+        </div>
+    )
+}
